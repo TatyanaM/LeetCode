f4bf3dcd52f69527dadb5c4a6bbf194a93b77b4d
Tatiana Mudryak <tatiana.mudryak@gmail.com>
2019-08-17 23:15:02 +0300

    Init

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by Tatiana Mudryak on 17/08/2019.
+//  Copyright © 2019 Tatiana Mudryak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
